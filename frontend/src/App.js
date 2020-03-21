8262f7528b377647d1eaed4c5e200b505ef012c5
Felix Dürrwald <mplabs@mplabs.de>
2020-03-21 16:21:33 +0100

    Add sass support and tent.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import Route from 'react-router-dom/Route';
-import Switch from 'react-router-dom/Switch';
+import { Route, Switch  }from 'react-router-dom';
 import Home from './Home';
-import './App.css';
+import './App.scss';
+import { ApolloProvider } from 'react-apollo';
+import { createClient } from  './lib/apollo'
+
+const client = createClient()
 
 const App = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-  </Switch>
+  <ApolloProvider client={client}>
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+  </ApolloProvider>
 );
 
 export default App;
