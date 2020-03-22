802ef0c2dcf4d2da8e255f4b5dd877e25a11eb9d
Marc_The_Shark <marc.piper@blindside.pro>
2020-03-22 16:58:35 +0100

    Added VideoCard component

---
+++ frontend/src/components/VideoCard/VideoCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './VideoCard.scss';
+
+class VideoCard extends React.Component {
+
+  render() {
+    <div className="video-card">
+
+    </div>
+  }
+}
+
+export default VideoCard
