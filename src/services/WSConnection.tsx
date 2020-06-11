3cdc11af4deaa9e4f689b1784541041daf8743e9
Halvard Mørstad <halvard@simplyundoable.com>
2020-06-11 19:38:47 +0200

    modified wsconnection according to backend

--- src/services/WSConnection.tsx
+++ src/services/WSConnection.tsx
@@ -1,117 +1,87 @@
-import { SOCKET_URL } from '../Constants'
+import Peer from "simple-peer";
+import {
+  Message,
+  PayloadConnections,
+  PayloadNewConnection,
+} from "../../../social-slam-streaming/lib/types";
+import { SOCKET_URL } from "../Constants";
 
 export const Connection = (streamId: string, token: string): WebSocket => {
-  const ws = new WebSocket(SOCKET_URL)
+  const ws = new WebSocket(SOCKET_URL);
 
   ws.onopen = () => {
-    ws.send(JSON.stringify({ type: 'connect', streamId, token }))
-  }
+    const payload: Message = {
+      type: "connect_to_room",
+      streamId,
+      token,
+    };
+    ws.send(JSON.stringify(payload));
+  };
 
   ws.onmessage = (msg: MessageEvent) => {
-    const message = JSON.parse(msg.data)
-
-    console.log(message)
-  }
+    const message = JSON.parse(msg.data);
+    console.log(message);
+    const [slammers, connections] = onWsMessage(ws, message);
+  };
 
   // @ts-ignore
   ws.onclose = (code: number, msg: string) => {
-    console.log(code, msg)
+    console.log(code, msg);
+  };
+
+  return ws;
+};
+
+const onWsMessage = (
+  ws: WebSocket,
+  msg: PayloadConnections | PayloadNewConnection,
+) => {
+  const peers: Peer.Instance[] = [];
+  const peersStreamers: Record<string, Peer.Instance> = {};
+  if (msg.type === "connections") {
+    msg.connections.forEach((el) => {
+      const peer = new Peer({
+        initiator: true,
+        trickle: false,
+      });
+
+      peer.on("signal", (signal) => {
+        const payload: Message = {
+          type: "send_signal",
+          connId: el,
+          signal: signal,
+          originId: msg.socketId,
+        };
+        ws.send(JSON.stringify(payload));
+      });
+
+      peersStreamers[el] = peer;
+      peers.push(peer);
+    });
+
+    return [peersStreamers, peers];
+  } else if (msg.type === "new_connection") {
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+    });
+    peer.on("signal", (signal) => {
+      const payload: Message = {
+        type: "confirm_signal",
+        signal: signal,
+        originId: msg.originId,
+      };
+      ws.send(JSON.stringify(payload));
+    });
+    peer.signal(msg.signal);
+
+    if (msg.isArtist) {
+      peersStreamers[msg.originId] = peer;
+      peers.push(peer);
+    }
+
+    return [peersStreamers, peers];
+  } else if (msg.type === "returned_signal") {
+    peersStreamers[msg.originId].signal(msg.signal);
   }
-
-  return ws
-}
-
-// import io from 'socket.io-client'
-
-// const socketConnection = ({ url, token, streamId }) => {
-//   const socketRef = io(url)
-
-//   socketRef.emit('connect_to_room', {
-//     streamId,
-//     token,
-//   })
-
-//   // artists: Set<string>, sockets: Set<string>
-//   socketRef.on('connections', (payload) => {
-//     const { artists, sockets } = payload
-//     const peers = []
-
-//     sockets.forEach((socketId) => {
-//       const peer = createPeer(socketId, socketRef.id)
-//       peers.push(peer)
-//       if (artists.contains(socketId)) {
-//         peersRef.push({
-//           socketId,
-//           peer,
-//         })
-//       }
-//     })
-
-//     setViewerCount(peers.length)
-//     setPeers(peers)
-//   })
-
-//   socketRef.on('message', (payload) => {
-//     if (payload.error) {
-//       console.error(payload)
-//     } else {
-//       console.info(payload)
-//     }
-//   })
-
-//   socketRef.on('new_connection', (payload) => {
-//     const peer = addPeer(payload.signal, payload.callerId)
-//     peers.push(peer)
-//     //         peersRef.push({
-//     //     socketId: payload.callerId,
-//     //     peer,
-//     //   })
-//   })
-
-//   socketRef.on('confirming_connection', (payload) => {
-//     const connection = peersRef.find((p) => p.socketId === payload.socketId)
-//     connection.peer.signal(payload.signal)
-//   })
-
-//   return socketRef
-// }
-
-// const createPeer = (socketId, callerId) => {
-//   const peer = new Peer({ initiator: true, trickle: false })
-
-//   peer.on('signal', (signal) => {
-//     socketRef.current.emit('send_signal', {
-//       socketId,
-//       callerId,
-//       signal,
-//     })
-//   })
-
-//   return peer
-// }
-
-// const addPeer = (incomingSignal, callerId) => {
-//   const peer = new Peer({
-//     initiator: false,
-//     trickle: false,
-//   })
-
-//   peer.on('signal', (signal) => {
-//     socketRef.current.emit('receive_signal', { signal, callerId })
-//   })
-
-//   peer.on('data', (newMsg) => {
-//     chatLog.push(newMsg)
-//     setChatLog(chatLog)
-//   })
-
-//   peer.signal(incomingSignal)
-
-//   return peer
-// }
-
-// const messagePeers = (newMsg) => {
-//   peers.forEach((peer) => {
-//     peer.send(newMsg)
-//   })
-// }
+};
