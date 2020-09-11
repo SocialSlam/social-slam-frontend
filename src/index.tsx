6eb7992b031e9590fc10bae6a6ef4a658ff4ba30
halvardssm <halvard@simplyundoable.com>
2020-09-11 19:54:24 +0200

    Updated CI, added browserlist, added regenerator runtime

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { App } from './App'
 import { store } from './redux/Store'
+import 'regenerator-runtime/runtime'
 
 const render = () => ReactDOM.render(<App />, document.getElementById('root'))
 
