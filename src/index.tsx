6b91e64f0a48a2a77b0520e1c183a6f6fe8cf59f
halvardssm <halvard@simplyundoable.com>
2020-09-12 19:00:35 +0200

    added core js

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
+import 'core-js';
+import 'regenerator-runtime/runtime'
+
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { App } from './App'
 import { store } from './redux/Store'
-import 'regenerator-runtime/runtime'
 
 const render = () => ReactDOM.render(<App />, document.getElementById('root'))
 
